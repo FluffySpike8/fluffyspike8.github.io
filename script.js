a6753743d2d9058701d74cfafe254a6c9c3604f1
Brent De Clercq <dcbrent8@gmail.com>
2025-03-16 14:01:08 +0100

    Even fancier hamburger

--- script.js
+++ script.js
@@ -14,7 +14,17 @@ document.getElementById("year").textContent = new Date().getFullYear();
 //   document.querySelector("nav").classList.toggle("nav-active");
 // }
 
-function toggleMenu() {
-  document.querySelector("nav").classList.toggle("nav-active");
-  document.querySelector(".hamburger").classList.toggle("active");
-}
+// function toggleMenu() {
+//   document.querySelector("nav").classList.toggle("nav-active");
+//   document.querySelector(".hamburger").classList.toggle("active");
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector("nav");
+
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("nav-active");
+  });
+});
